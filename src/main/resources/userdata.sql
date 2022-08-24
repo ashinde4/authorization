afa8c6570d6005e99aa582fbdcc1f53d94ce3d58
Shinde <858471@cognizant.com>
2022-08-24 15:07:35 +0530

    first commit

---
+++ src/main/resources/userdata.sql
@@ -0,0 +1,3 @@
+INSERT INTO userdata (user_id,user_password,user_name) VALUES ('admin','admin','admin');
+INSERT INTO userdata (user_id,user_password,user_name) VALUES ('admin1','admin1','admin1');
+INSERT INTO userdata (user_id,user_password,user_name) VALUES ('AkshayS','AkshayS','Akshay S');
